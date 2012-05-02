c51f2e3e214acd7f467e73893558441e508e0b4b
kejar31 <justin.a.rogers@gmail.com>
2012-05-02 16:13:58 -0500

    fixed incorrect dir name

--- full_stingray.mk
+++ full_stingray.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/moto/stingray/device.mk)
+$(call inherit-product, device/motorola/stingray/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_stingray
